a80e3572ed69a560f682e68b653e791855ab5344
pmdkr <pramodkrlohra@gmail.com>
2025-12-23 19:58:28 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+let message: string = "hello world from Typescript";
+
+console.log(message);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(`2 + 3 = ${add(2, 3)}`);
